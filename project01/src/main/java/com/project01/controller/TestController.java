0746a79da7ca77decee1f3bf40e13cb85a9c342d
kyoungmin1215 <wkm2595@gmail.com>
2023-01-27 19:56:20 +0900

    first commit

---
+++ project01/src/main/java/com/project01/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.project01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class TestController {
+	
+//	@GetMapping("/testGetParameter")
+//	public String testGetParameter(@ModelAttribute("name") String name) {
+//		System.out.println("전달되는 값 : " + name);
+//		
+//		return "testGet";
+//	}
+
+	@RequestMapping("/testGetParameter/{name}")
+	public String testGetParameter(@PathVariable String name) {
+		System.out.println(name);
+		
+		return "testGet";
+	}
+}
---
+++ project01/src/main/webapp/WEB-INF/views/testGet.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	전달되는 값 : ${name }<hr>
+</body>
+</html>
