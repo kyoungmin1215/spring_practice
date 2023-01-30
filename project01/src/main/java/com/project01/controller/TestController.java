32f7493789d00cd73eab761e6887c4f6c3e66ffd
kyoungmin1215 <wkm2595@gmail.com>
2023-01-30 22:39:15 +0900

    test commit

--- project01/src/main/java/com/project01/controller/TestController.java
+++ project01/src/main/java/com/project01/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 	
 //	@GetMapping("/testGetParameter")
 //	public String testGetParameter(@ModelAttribute("name") String name) {
-//		System.out.println("전달되는 값 : " + name);
+//		System.out.println("name : " + name);
 //		
 //		return "testGet";
 //	}
