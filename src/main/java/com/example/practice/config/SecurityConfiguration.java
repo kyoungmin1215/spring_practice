5240573dc0a695e74e65f1dafdb24f043bbffa7e
kyoungmin1215 <wkm2595@gmail.com>
2023-02-18 20:29:27 +0900

    change to relative path

--- src/main/java/com/example/practice/config/SecurityConfiguration.java
+++ src/main/java/com/example/practice/config/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        Reader yamlFile = new FileReader("src//main//resources//properties//securityUserInfo.yml");
+        Reader yamlFile = new FileReader("./src/main/resources/properties/securityUserInfo.yml");
         Map<String, ArrayList> yamlMaps = new Yaml().load(yamlFile);
 
         for(int i=0; i<yamlMaps.get("user").size(); i++) {
